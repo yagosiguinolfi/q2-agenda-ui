c4659cc3f9e4b240f1e3ac90df15ccc81980f67d
Yago Siguinolfi <yagosiguinolfi@gmail.com>
2023-05-25 13:17:42 -0300

    Base project

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const Logo = require('./assets/logo-azul.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={Logo} />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { ExpoConfig } from 'expo/config';
+
+// In SDK 46 and lower, use the following import instead:
+// import { ExpoConfig } from '@expo/config-types';
+
+const config: ExpoConfig = {
+  name: 'q2-agenda-ui-app',
+  slug: 'q2-agenda-ui-app',
+};
+
+export default config;
