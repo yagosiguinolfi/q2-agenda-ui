ecd0291aa97f9f0aeff1a47e1fa56b67839929b1
Yago Siguinolfi <yagosiguinolfi@gmail.com>
2023-06-02 18:34:39 -0300

    Authentication classes and dependences

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
+import "react-native-gesture-handler";
 import React from 'react';
 import { Routes } from './src/routes';
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <Routes />
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
 
